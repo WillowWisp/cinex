9b2c8195422514b3b6131365d51efc9d0508a9d9
tligodsp <43437080+tligodsp@users.noreply.github.com>
2019-12-21 23:45:25 +0700

    Finished Schedule Screen

--- src/layout/pages/Schedule/components/MoviesSchedule/MoviesSchedule.jsx
+++ src/layout/pages/Schedule/components/MoviesSchedule/MoviesSchedule.jsx
@@ -2,49 +2,112 @@ import React, {useState, useEffect} from 'react';
 import { Container } from 'react-bootstrap';
 import classes from './MoviesSchedule.module.scss';
 
-import { mockScreenType } from '../../../../../mock-data';
+import {helper} from '../../../../../utils/helper';
+
+import { mockScreenType, mockNowOnMovies, mockShowtimes } from '../../../../../mock-data';
 
 const MoviesSchedule = () => {
-  const [choosenScreenTypes, setChoosenScreenTypes] = useState([]);
+  const [chosenScreenTypes, setChosenScreenTypes] = useState([]);
+  const [moviesChosenDate, setMoviesChosenDate] = useState([]);
 
   useEffect(() => {
-    setChoosenScreenTypes(
+    setChosenScreenTypes(
       Array.from(mockScreenType, (screenType) => (screenType.id))
     );
-    }, []
-  );
+    const today = new Date();
+    setMoviesChosenDate(
+      Array.from(mockNowOnMovies, (movie) => ({ movieId: movie.id, chosenDate: today.getDate() }))
+    );
+    getShowtimesByMovieIdAndDate('movie_00', 21);
+  }, []);
 
-  const isChoosenScreenType = (screenType) => {
-    return choosenScreenTypes.findIndex(choosen => choosen === screenType.id) > -1;
+  const getDatesToDisplay = () => {
+    const MS_IN_A_DAY = 86400000;
+    const today = new Date();
+    today.setHours(0,0,0,0);
+    const datesToDisplay = [
+      today,
+      new Date(today.getTime() + MS_IN_A_DAY * 1),
+      new Date(today.getTime() + MS_IN_A_DAY * 2),
+      new Date(today.getTime() + MS_IN_A_DAY * 3),
+      new Date(today.getTime() + MS_IN_A_DAY * 4),
+    ];
+    return datesToDisplay;
   }
 
-  const isAllScreenTypesChoosen = () => {
-    return choosenScreenTypes.length === mockScreenType.length;
+  const getShowtimesByMovieIdAndDate = (movieId, date) => {
+    // TODO: Đưa cho hoàng tử Chăm pa xử lí ở backend
+    var showtimeByScreenType = Array.from(mockScreenType, (screenType => ({ screenType: screenType, showtimes: [] })));
+    for (var showtime of mockShowtimes) {
+      const showDate = new Date(showtime.startAt);
+      if (showtime.movieId === movieId && showDate.getDate() === date) {
+        const screenTypeIndex = showtimeByScreenType.findIndex(st => st.screenType.name === showtime.screenType);
+        showtimeByScreenType[screenTypeIndex].showtimes.push(showtime);
+      }
+    }
+    showtimeByScreenType = showtimeByScreenType.filter(st => st.showtimes.length > 0);
+    console.log(showtimeByScreenType);
+    return showtimeByScreenType;
   }
+
+  const getChosenDateOfMovie = (movieId) => {
+    console.log(movieId);
+    const index = moviesChosenDate.findIndex(chosen => {
+      return chosen.movieId === movieId
+    });
+    console.log(index);
+
+    return moviesChosenDate[index].chosenDate;
+  }
+
+  const isChosenScreenType = (screenType) => {
+    return chosenScreenTypes.findIndex(chosen => chosen === screenType.id) > -1;
+  }
+
+  const isAllScreenTypesChosen = () => {
+    return chosenScreenTypes.length === mockScreenType.length;
+  }
+
+  const isChosenDateOfMovie = (movieId, date) => {
+    const movieIndex = moviesChosenDate.findIndex(chosen => chosen.movieId === movieId);
+    console.log(date === moviesChosenDate[movieIndex].chosenDate);
+    return date === moviesChosenDate[movieIndex].chosenDate;
+  }
+
 
   const onScreenTypeButtonClick = (screenType) => {
-    if (isChoosenScreenType(screenType)) {
-      if (choosenScreenTypes.length <= 1) {
+    if (isChosenScreenType(screenType)) {
+      if (chosenScreenTypes.length <= 1) {
         return;
       }
-      setChoosenScreenTypes(choosenScreenTypes.filter(choosen => choosen !== screenType.id));
+      setChosenScreenTypes(chosenScreenTypes.filter(chosen => chosen !== screenType.id));
     }
     else {
-      setChoosenScreenTypes([ ...choosenScreenTypes, screenType.id ]);
+      setChosenScreenTypes([ ...chosenScreenTypes, screenType.id ]);
     }
   }
 
   const onAllScreenTypeClick = () => {
-    setChoosenScreenTypes(
+    setChosenScreenTypes(
       Array.from(mockScreenType, (screenType) => (screenType.id))
     );
+  }
+
+  const onDateClick = (movieId, date) => {
+    setMoviesChosenDate(
+      moviesChosenDate.map(chosen => (
+        chosen.movieId === movieId
+        ? { ...chosen, chosenDate: date }
+        : chosen
+      ))
+    )
   }
 
   const renderScreenTypeButton = (screenType) => {
     return (
       <div 
         className={ 
-          isChoosenScreenType(screenType)
+          isChosenScreenType(screenType)
           ? classes['screen-type-button-active'] 
           : classes['screen-type-button']
         }
@@ -59,7 +122,7 @@ const MoviesSchedule = () => {
       <div className={classes['screen-types']}>
       <div 
         className={ 
-          isAllScreenTypesChoosen()
+          isAllScreenTypesChosen()
           ? classes['screen-type-button-active'] 
           : classes['screen-type-button']
         }
@@ -73,14 +136,97 @@ const MoviesSchedule = () => {
     );
   }
 
+  const renderMovie = (movie) => {
+    const showtimes = moviesChosenDate.length > 0 
+          ? getShowtimesByMovieIdAndDate(movie.id, getChosenDateOfMovie(movie.id))
+          : [];
+    return (
+      <div className={classes['movie-container']}>
+        <div className={classes['movie-info-and-display-dates']}>
+          <div className={classes['poster-container']}>
+            <img className={classes["poster"]} src={movie.poster} alt="movie-poster"/>
+          </div>
+          <div className={classes['info-and-display-dates']}>
+            <div className={classes['info']}>
+              <div className={classes['title']}>{movie.title}</div>
+              <div className={classes['sub-info']}>{movie.runtime} min  |  {helper.getFormattedGenresString(movie.genres)}</div>
+            </div>
+            <div className={classes['display-dates']}>
+              {getDatesToDisplay().map(date => (
+                <div className={classes['date-container']}>
+                  <div 
+                    className={
+                      classes['date-clickable'] +
+                      (chosenScreenTypes.length > 0 && isChosenDateOfMovie(movie.id, date.getDate())
+                      ? (' ' + classes['date-clickable-active'])
+                      : '')
+                    }
+                    onClick={onDateClick.bind(this, movie.id, date.getDate())}
+                  >
+                    <div className={classes['day-of-week']}>{helper.getDayOfWeekName(date.getDay())}</div>
+                    <div className={classes['day-of-month']}>{date.getDate()}</div>
+                    <div className={classes['month']}>{helper.getMonthName(date.getMonth())}</div>
+                  </div>
+                  <div className={classes['date-foot']}>
+                    { chosenScreenTypes.length > 0 && isChosenDateOfMovie(movie.id, date.getDate())
+                      ? <div className={classes['foot-circle']}></div>
+                      : <div className={classes['foot-line']}></div>
+                    }
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        {
+          moviesChosenDate.length > 0
+          ? (
+            <div className={classes['screen-type-available']}>
+              {showtimes.map((showtimeByScreentype, index) => (
+                <div className={classes['show-time-by-screen-type']}>
+                  <div className={classes['screen-type-container']}>
+                    {showtimeByScreentype.screenType.name}
+                  </div>
+                  <div className={classes['showtimes-container']}>
+                    {showtimeByScreentype.showtimes.map(showtime => (
+                      <div className={classes['showtime']}>
+                        {helper.getFormattedTime(new Date(showtime.startAt))}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              ))}
+              {showtimes.length <= 0
+                ? (
+                  <div className={classes['no-showtime']}>
+                    No Showtime on the chosen Date
+                  </div>
+                )
+                : null
+              }
+            </div>
+          )
+          : null
+        }
+      </div>
+    );
+  }
+
   return (
     <Container className={classes['container']}>
+
       <div className={classes['header']}>
         <div className={classes['big-text']}>
           Lịch chiếu
         </div>
         <div className={classes['fading-line']}></div>
         {renderScreenTypes(mockScreenType)}
+      </div>
+
+      <div className={classes['movies']}>
+        { mockNowOnMovies.map(movie => (
+          renderMovie(movie)
+        )) }
       </div>
     </Container>
   );
--- src/mock-data.js
+++ src/mock-data.js
@@ -16,3 +16,215 @@ export const mockScreenType = [
     name: 'CINEMA 4D',
   },
 ]
+
+const mockGenres = [
+  {
+    id: 'genre_00',
+    name: 'Crime',
+  },
+  {
+    id: 'genre_01',
+    name: 'Drama',
+  },
+  {
+    id: 'genre_02',
+    name: 'Thriller',
+  },
+  {
+    id: 'genre_03',
+    name: 'Action',
+  },
+  {
+    id: 'genre_04',
+    name: 'Adventure',
+  },
+  {
+    id: 'genre_05',
+    name: 'Sci-fi',
+  },
+  {
+    id: 'genre_06',
+    name: 'Animation',
+  },
+]
+
+const movieJoker = {
+  id: 'movie_00',
+  poster: 'https://m.media-amazon.com/images/M/MV5BNGVjNWI4ZGUtNzE0MS00YTJmLWE0ZDctN2ZiYTk2YmI3NTYyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg',
+  title: 'Gã hề dí dỏm',
+  runtime: 122,
+  genres: []
+}
+
+export const mockNowOnMovies = [
+  {
+    id: 'movie_00',
+    poster: 'https://m.media-amazon.com/images/M/MV5BNGVjNWI4ZGUtNzE0MS00YTJmLWE0ZDctN2ZiYTk2YmI3NTYyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg',
+    title: 'Gã hề dí dỏm',
+    runtime: 122,
+    genres: [ mockGenres[0], mockGenres[1], mockGenres[2] ],
+  },
+  {
+    id: 'movie_01',
+    poster: 'https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
+    title: 'Batman v Superman: Dawn of Justice',
+    runtime: 151,
+    genres: [ mockGenres[3], mockGenres[4], mockGenres[5] ],
+  },
+  {
+    id: 'movie_02',
+    poster: 'https://m.media-amazon.com/images/M/MV5BZmUxZmVlNGMtZGMyMy00MmM3LTg5ZjgtNzFhZWU4MTU5MjIwXkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_SY1000_CR0,0,666,1000_AL_.jpg',
+    title: 'Code Geass: Lelouch of the Re;Surrection',
+    runtime: 112,
+    genres: [ mockGenres[3], mockGenres[4], mockGenres[6] ],
+  },
+  {
+    id: 'movie_03',
+    poster: 'https://m.media-amazon.com/images/M/MV5BMjMwNDkxMTgzOF5BMl5BanBnXkFtZTgwNTkwNTQ3NjM@._V1_SY1000_CR0,0,674,1000_AL_.jpg',
+    title: 'Spider-Man: Into the Spider-Verse',
+    runtime: 117,
+    genres: [ mockGenres[3], mockGenres[4], mockGenres[6] ],
+  },
+]
+
+// showtime {
+//   -id: string
+//   -movieId: string
+//   -startAt: yyyy-MM-ddThh:mm:ss.SSS
+//   -screenType: string
+//   }
+  
+export const mockShowtimes = [
+  {
+    id: 'showtime_00',
+    movieId: 'movie_00',
+    startAt: '2019-12-21T11:30:00.000',
+    screenType: '2D',
+  },
+  {
+    id: 'showtime_01',
+    movieId: 'movie_00',
+    startAt: '2019-12-21T15:30:00.000',
+    screenType: '2D',
+  },
+  {
+    id: 'showtime_02',
+    movieId: 'movie_00',
+    startAt: '2019-12-21T21:00:00.000',
+    screenType: '2D',
+  },
+  {
+    id: 'showtime_03',
+    movieId: 'movie_00',
+    startAt: '2019-12-22T9:30:00.000',
+    screenType: '2D',
+  },
+  {
+    id: 'showtime_04',
+    movieId: 'movie_00',
+    startAt: '2019-12-22T13:30:00.000',
+    screenType: '2D',
+  },
+  {
+    id: 'showtime_05',
+    movieId: 'movie_00',
+    startAt: '2019-12-22T18:00:00.000',
+    screenType: '2D',
+  },
+  {
+    id: 'showtime_06',
+    movieId: 'movie_00',
+    startAt: '2019-12-21T9:30:00.000',
+    screenType: 'IMAX',
+  },
+  {
+    id: 'showtime_07',
+    movieId: 'movie_00',
+    startAt: '2019-12-21T13:30:00.000',
+    screenType: 'IMAX',
+  },
+  {
+    id: 'showtime_08',
+    movieId: 'movie_00',
+    startAt: '2019-12-21T18:00:00.000',
+    screenType: 'IMAX',
+  },
+  {
+    id: 'showtime_09',
+    movieId: 'movie_01',
+    startAt: '2019-12-21T18:00:00.000',
+    screenType: '3D',
+  },
+  {
+    id: 'showtime_10',
+    movieId: 'movie_02',
+    startAt: '2019-12-22T13:00:00.000',
+    screenType: '3D',
+  },
+  {
+    id: 'showtime_11',
+    movieId: 'movie_01',
+    startAt: '2019-12-22T11:30:00.000',
+    screenType: '2D',
+  },
+  {
+    id: 'showtime_12',
+    movieId: 'movie_01',
+    startAt: '2019-12-22T15:30:00.000',
+    screenType: '2D',
+  },
+  {
+    id: 'showtime_13',
+    movieId: 'movie_01',
+    startAt: '2019-12-22T21:00:00.000',
+    screenType: '2D',
+  },
+  {
+    id: 'showtime_14',
+    movieId: 'movie_01',
+    startAt: '2019-12-23T9:30:00.000',
+    screenType: '2D',
+  },
+  {
+    id: 'showtime_15',
+    movieId: 'movie_01',
+    startAt: '2019-12-23T13:30:00.000',
+    screenType: '2D',
+  },
+  {
+    id: 'showtime_16',
+    movieId: 'movie_01',
+    startAt: '2019-12-23T18:00:00.000',
+    screenType: '2D',
+  },
+  {
+    id: 'showtime_17',
+    movieId: 'movie_01',
+    startAt: '2019-12-22T9:30:00.000',
+    screenType: 'IMAX',
+  },
+  {
+    id: 'showtime_18',
+    movieId: 'movie_01',
+    startAt: '2019-12-22T13:30:00.000',
+    screenType: 'IMAX',
+  },
+  {
+    id: 'showtime_19',
+    movieId: 'movie_01',
+    startAt: '2019-12-22T18:00:00.000',
+    screenType: 'IMAX',
+  },
+  {
+    id: 'showtime_20',
+    movieId: 'movie_02',
+    startAt: '2019-12-22T18:00:00.000',
+    screenType: '3D',
+  },
+  {
+    id: 'showtime_21',
+    movieId: 'movie_03',
+    startAt: '2019-12-23T13:00:00.000',
+    screenType: '3D',
+  },
+]
